3084e68ac5f812fe22fa44b0d87bbfde7693b164
Anik Sarkar <anik.sarkar@duronto.tv>
2025-11-25 11:00:31 +0600

    form completed

--- frontend/src/Dashboard/components/LeavePage.jsx
+++ frontend/src/Dashboard/components/LeavePage.jsx
@@ -61,63 +61,33 @@ const LeavePage = () => {
 
           {/* Details of Requesting Leave */}
           <div className="section-header">Details of Requesting Leave</div>
-
-          <table className="form-table details-table">
-            <tbody>
-              <tr>
-                <td rowSpan={3} className="label-cell" style={{width: '15%'}}>No. of Day(s)</td>
-                <td rowSpan={3} style={{width: '12%', textAlign: 'center'}}>
-                  <Input bordered={false} className="form-input center-input" />
-                  <div className="sub-label">day(s)</div>
-                </td>
-                <td className="label-cell" style={{width: '10%'}}>From:</td>
-                <td style={{width: '23%'}}>
-                  <Input bordered={false} className="form-input" />
-                </td>
-                <td className="label-cell" style={{width: '10%'}}>To:</td>
-                <td style={{width: '30%'}}>
-                  <Input bordered={false} className="form-input" />
-                </td>
-              </tr>
-              <tr>
-                <td className="checkbox-cell">
-                  <Checkbox /> Morning
-                </td>
-                <td></td>
-                <td className="checkbox-cell">
-                  <Checkbox /> Evening
-                </td>
-                <td></td>
-              </tr>
-              <tr>
-                <td className="checkbox-cell">
-                  <Checkbox defaultChecked /> Sick
-                </td>
-                <td className="checkbox-cell">
-                  <Checkbox /> Annual
-                </td>
-                <td className="checkbox-cell">
-                  <Checkbox /> Replacement
-                </td>
-                <td className="checkbox-cell">
-                  <Checkbox /> Without Pay
-                </td>
-              </tr>
-            </tbody>
-          </table>
-
           <table className="form-table">
             <tbody>
               <tr>
-                <td className="label-cell" style={{width: '20%'}}>If Half Day</td>
+                <td className="label-cell" style={{ width: '15%' }}>No. of Day(s)</td>
+                <td style={{ width: '12%', textAlign: 'center' }}>
+                  <Input bordered={false} className="form-input center-input" />
+                  <div className="sub-label">day(s)</div>
+                </td>
+                <td className="label-cell" style={{ width: '10%' }}>From:</td>
+                <td style={{ width: '23%' }}>
+                  <Input bordered={false} className="form-input" />
+                </td>
+                <td className="label-cell" style={{ width: '10%' }}>To:</td>
+                <td style={{ width: '30%' }}>
+                  <Input bordered={false} className="form-input" />
+                </td>
+              </tr>
+              <tr>
+                <td className="label-cell" style={{ width: '20%' }}>If Half Day</td>
                 <td className="checkbox-cell">
-                   Not Required <Checkbox />
+                  Not Required <Checkbox />
                 </td>
                 <td className="checkbox-cell">
-                   Morning <Checkbox />
+                  Morning <Checkbox />
                 </td>
                 <td className="checkbox-cell">
-                   Evening <Checkbox />
+                  Evening <Checkbox />
                 </td>
                 <td className="checkbox-cell"></td>
                 <td className="checkbox-cell"></td>
@@ -125,19 +95,19 @@ const LeavePage = () => {
               <tr>
                 <td className="label-cell">Leave Type</td>
                 <td className="checkbox-cell">
-                   Casual <Checkbox />
+                  Casual <Checkbox />
                 </td>
                 <td className="checkbox-cell">
-                   Sick <Checkbox />
+                  Sick <Checkbox />
                 </td>
                 <td className="checkbox-cell">
-                   Annual <Checkbox />
+                  Annual <Checkbox />
                 </td>
                 <td className="checkbox-cell">
-                   Replacement <Checkbox />
+                  Replacement <Checkbox />
                 </td>
                 <td className="checkbox-cell">
-                   Without Pay <Checkbox />
+                  Without Pay <Checkbox />
                 </td>
               </tr>
               <tr>
@@ -152,24 +122,24 @@ const LeavePage = () => {
                   <Input bordered={false} className="form-input" /> day(s)
                 </td>
                 <td className="checkbox-cell">
-                   <Input bordered={false} className="form-input" /> day(s)
+                  <Input bordered={false} className="form-input" /> day(s)
                 </td>
                 <td className="checkbox-cell">
                   <Input bordered={false} className="form-input" /> day(s)
                 </td>
               </tr>
               <tr>
-                <td className="label-cell">Station<br/>(During Leave) :</td>
+                <td className="label-cell">Station<br />(During Leave) :</td>
                 <td colSpan={2}>
                   <Input bordered={false} className="form-input" />
                 </td>
-                <td className="label-cell">Contact<br/>(During Leave) :</td>
+                <td className="label-cell">Contact<br />(During Leave) :</td>
                 <td colSpan={2}>
                   <Input bordered={false} className="form-input" />
                 </td>
               </tr>
               <tr>
-                <td className="label-cell" style={{verticalAlign: 'top'}}>Person In-Charge<br/>(During My Leave) :</td>
+                <td className="label-cell" style={{ verticalAlign: 'top' }}>Person In-Charge<br />(During My Leave) :</td>
                 <td colSpan={5}>
                   <Input bordered={false} className="form-input" />
                   <div className="signature-line">Signature</div>
@@ -183,7 +153,7 @@ const LeavePage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="label-cell" style={{verticalAlign: 'top'}}>Reason of Leave :</td>
+                <td className="label-cell" style={{ verticalAlign: 'top' }}>Reason of Leave :</td>
                 <td colSpan={5}>
                   <Input.TextArea bordered={false} rows={3} className="form-textarea" />
                   <div className="signature-section">
@@ -199,17 +169,17 @@ const LeavePage = () => {
             <tbody>
               <tr>
                 <td className="approval-cell">
-                  <div className="approval-title">Approved by Departmental Head</div>
+                  <div className="section-header">Approved by Departmental Head</div>
                   <div className="approval-space"></div>
                   <div className="approval-date">Date:</div>
                 </td>
                 <td className="approval-cell">
-                  <div className="approval-title">Approved by Final Authority</div>
+                  <div className="section-header">Approved by Final Authority</div>
                   <div className="approval-space"></div>
                   <div className="approval-date">Date:</div>
                 </td>
                 <td className="approval-cell">
-                  <div className="approval-title">Endorsed by HR Department</div>
+                  <div className="section-header">Endorsed by HR Department</div>
                   <div className="approval-space"></div>
                   <div className="approval-date">Date:</div>
                 </td>
@@ -220,7 +190,7 @@ const LeavePage = () => {
       </div>
 
       {/* APPLICANT COPY */}
-      <div className="leave-form-container" style={{marginTop: '40px'}}>
+      <div className="leave-form-container" style={{ marginTop: '40px' }}>
         <div className="form-header">
           <div className="company-logo">Renaissance</div>
           <div className="company-info">
@@ -241,28 +211,22 @@ const LeavePage = () => {
         <table className="form-table">
           <tbody>
             <tr>
-              <td className="label-cell" style={{width: '15%'}}>Date:</td>
-              <td style={{width: '35%'}}>
-                <Input bordered={false} className="form-input" />
-              </td>
-              <td className="label-cell" style={{width: '20%'}}>Employee ID :</td>
-              <td style={{width: '30%'}}>
-                <Input bordered={false} className="form-input" />
-              </td>
-            </tr>
-            <tr>
               <td className="label-cell">Applicant Name :</td>
               <td>
                 <Input bordered={false} className="form-input" />
               </td>
-              <td className="label-cell">Department :</td>
+              <td className="label-cell">Employee ID :</td>
               <td>
                 <Input bordered={false} className="form-input" />
               </td>
             </tr>
             <tr>
               <td className="label-cell">Designation :</td>
-              <td colSpan={3}>
+              <td>
+                <Input bordered={false} className="form-input" />
+              </td>
+              <td className="label-cell">Department :</td>
+              <td>
                 <Input bordered={false} className="form-input" />
               </td>
             </tr>
@@ -274,88 +238,81 @@ const LeavePage = () => {
         <table className="form-table details-table">
           <tbody>
             <tr>
-              <td rowSpan={3} className="label-cell" style={{width: '15%'}}>No. of Day(s)</td>
-              <td rowSpan={3} style={{width: '12%', textAlign: 'center'}}>
-                <Input bordered={false} className="form-input center-input" />
-                <div className="sub-label">day(s)</div>
-              </td>
-              <td className="label-cell" style={{width: '10%'}}>From:</td>
-              <td style={{width: '23%'}}>
-                <Input bordered={false} className="form-input" />
-              </td>
-              <td className="label-cell" style={{width: '10%'}}>To:</td>
-              <td style={{width: '30%'}}>
-                <Input bordered={false} className="form-input" />
-              </td>
-            </tr>
-            <tr>
-              <td className="checkbox-cell">
-                <Checkbox /> Morning
-              </td>
-              <td></td>
-              <td className="checkbox-cell">
-                <Checkbox /> Evening
-              </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td className="checkbox-cell">
-                <Checkbox /> Sick
-              </td>
-              <td className="checkbox-cell">
-                <Checkbox /> Annual
-              </td>
-              <td className="checkbox-cell">
-                <Checkbox /> Replacement
-              </td>
-              <td className="checkbox-cell">
-                <Checkbox /> Without Pay
-              </td>
-            </tr>
+                <td className="label-cell" style={{ width: '15%' }}>No. of Day(s)</td>
+                <td style={{ width: '12%', textAlign: 'center' }}>
+                  <Input bordered={false} className="form-input center-input" />
+                  <div className="sub-label">day(s)</div>
+                </td>
+                <td className="label-cell" style={{ width: '10%' }}>From:</td>
+                <td style={{ width: '23%' }}>
+                  <Input bordered={false} className="form-input" />
+                </td>
+                <td className="label-cell" style={{ width: '10%' }}>To:</td>
+                <td style={{ width: '30%' }}>
+                  <Input bordered={false} className="form-input" />
+                </td>
+              </tr>
+              <tr>
+                <td className="label-cell" style={{ width: '20%' }}>If Half Day</td>
+                <td className="checkbox-cell">
+                  Not Required <Checkbox />
+                </td>
+                <td className="checkbox-cell">
+                  Morning <Checkbox />
+                </td>
+                <td className="checkbox-cell">
+                  Evening <Checkbox />
+                </td>
+                <td className="checkbox-cell"></td>
+                <td className="checkbox-cell"></td>
+              </tr>
+              <tr>
+                <td className="label-cell">Leave Type</td>
+                <td className="checkbox-cell">
+                  Casual <Checkbox />
+                </td>
+                <td className="checkbox-cell">
+                  Sick <Checkbox />
+                </td>
+                <td className="checkbox-cell">
+                  Annual <Checkbox />
+                </td>
+                <td className="checkbox-cell">
+                  Replacement <Checkbox />
+                </td>
+                <td className="checkbox-cell">
+                  Without Pay <Checkbox />
+                </td>
+              </tr>
+              <tr>
+                <td className="label-cell">Available Leave</td>
+                <td>
+                  <Input bordered={false} className="form-input" /> day(s)
+                </td>
+                <td>
+                  <Input bordered={false} className="form-input" /> day(s)
+                </td>
+                <td>
+                  <Input bordered={false} className="form-input" /> day(s)
+                </td>
+                <td className="checkbox-cell">
+                  <Input bordered={false} className="form-input" /> day(s)
+                </td>
+                <td className="checkbox-cell">
+                  <Input bordered={false} className="form-input" /> day(s)
+                </td>
+              </tr>
+              <tr></tr>
           </tbody>
         </table>
 
         <table className="form-table">
           <tbody>
             <tr>
-              <td className="label-cell" style={{width: '15%'}}>If Half Day</td>
-              <td style={{width: '12%'}}>
-                <Checkbox defaultChecked /> Not Required
-              </td>
-              <td style={{width: '10%', textAlign: 'center'}}>
-                <Input bordered={false} className="form-input small-input" defaultValue="0" />
-                <div className="sub-label">day(s)</div>
-              </td>
-              <td style={{width: '23%'}}></td>
-              <td style={{width: '10%', textAlign: 'center'}}>
-                <Input bordered={false} className="form-input small-input" defaultValue="0" />
-                <div className="sub-label">days</div>
-              </td>
-              <td style={{width: '30%'}}></td>
-            </tr>
-            <tr>
-              <td className="label-cell">Leave Type</td>
-              <td className="checkbox-cell">
-                <Checkbox /> Casual
-              </td>
-              <td style={{textAlign: 'center'}}>
-                <Input bordered={false} className="form-input small-input" defaultValue="0" />
-                <div className="sub-label">day(s)</div>
-              </td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td className="label-cell">Available Leave</td>
-              <td style={{textAlign: 'center'}}>
-                <Input bordered={false} className="form-input small-input" defaultValue="0" />
-                <div className="sub-label">day(s)</div>
-              </td>
               <td colSpan={4} className="endorsement-section">
                 <div className="endorsed-text">Endorsed by HR Department</div>
                 <div className="name-field">
-                  <div>Name:</div>
+                  <div>Date:</div>
                 </div>
               </td>
             </tr>
@@ -363,7 +320,7 @@ const LeavePage = () => {
         </table>
 
         <div className="footer-info">
-          <div>Date:</div>
+          
           <div className="issue-info">
             <div>Issue Date: July 27, 2021</div>
             <div>Ref.: BML/HRF/V1/010/21</div>
